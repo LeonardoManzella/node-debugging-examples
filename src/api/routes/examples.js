4039bf0e0f323c7a0dc4823549027e7a500f1cea
LeonardoManzella <leonardo.manzella@gmail.com>
2020-09-28 11:00:19 -0300

    Added asynchronous error

--- src/api/routes/examples.js
+++ src/api/routes/examples.js
@@ -36,4 +36,14 @@ module.exports = (app) => {
       next(error);
     }
   });
+
+  router.get("/asynchronous-error", async (req, res, next) => {
+    logger.silly(`Called /examples/asynchronous-error`);
+    try {
+      const data = req.body;
+      res.status(200).json(SERVICES.asynchronousError());
+    } catch (error) {
+      next(error);
+    }
+  });
 };
--- src/utils/services.js
+++ src/utils/services.js
@@ -15,7 +15,22 @@ const ndbError = () => {
   return "Backend is working correctly";
 };
 
+const asynchronousError = () => {
+  let internalFlag = false;
+  setTimeout(() => {
+    throw new Error("async error");
+    //Note that this error is extremely difficult to find without race and clarify packages
+
+    //Do some stuff.. then change a variable
+    internalFlag = true;
+  }, 0);
+  return internalFlag
+    ? "Backend is working correctly"
+    : "Check the terminal, there is an async error to fix. Note that the server has crashed and you need to restart to retry again";
+};
+
 module.exports = {
   chromeDevToolsError,
   ndbError,
+  asynchronousError,
 };
